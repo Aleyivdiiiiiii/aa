e109ecc66801f7c6a6e7de7701ef72eea4a63899
husnu ehedov <82249578+sirincay2@users.noreply.github.com>
2021-05-25 18:09:37 +0400

    Update MildSwearing.js

--- plugin/MildSwearing.js
+++ plugin/MildSwearing.js
@@ -3,7 +3,7 @@
 const { Composer } = require('telegraf')
 
 const badWordsBan = [
-    /pasif(\s+)?(var(\s+)?mı|arıyorum|im)/i, // pasif
+    /pasif(\s+)?(var(\s+)?mı|arıyorum|im)/i, //s...
     /(?:^|[^\wığüşöçĞÜŞÖÇI])[gG]+(\s+)?[aA]+(\s+)?[vVwW]+(\s+)?[aA]+(\s+)?[tT]+/i, // gavat
     /(?:^|[^\wığüşöçĞÜŞÖÇI])[aA]+(\s+)?[mM]+(\s+)?([ıIiİl]+)?(\s+)?[kKqQgGğĞ]+/i, // amk
     /(?:^|[^\wİığüşöçĞÜŞÖÇI])[mM]+(\s+)?[qkK]+/i, // mk
@@ -14,8 +14,8 @@ module.exports = Composer.match(badWordsBan, async (ctx, next) => {
     if (ctx.chat.type.endsWith('group')) {
             if (!ctx.from._is_in_admin_list) {
 
-                await ctx.deleteMessage()                       //Küfürlü mesajı siler.
-                //await ctx.kickChatMember(ctx.from.id)         //Kişiyi yasaklar açmak için başındaki // işareti silin.
+                await ctx.deleteMessage()                       //Söyüşlü mesajı silər
+                //await ctx.kickChatMember(ctx.from.id)         //kick koduyla isə qrupdan atar
 
             }
     }
